6ff5d339bb1b7a041c00063f25df4ffcad0fbdcc
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-03-10 22:04:02 +0530

    Time: 2 ms (39.93%) | Memory: 9.9 MB (57.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -9,12 +9,13 @@ public:
             if(!isalnum(s[l]))
             {
                 l++;
-                continue;
+                // continue;
+                // break;
             }
             else if(!isalnum(s[r]))
             {
                 r--;
-                continue;
+                // continue;
             }
             else if(tolower(s[l]) != tolower(s[r])) return false; // only bracket was missing
             else
