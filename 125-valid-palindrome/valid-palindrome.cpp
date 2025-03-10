d6121dbf6c069e9da4063c212eaac25681755583
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-03-10 22:03:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (41.87%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int n = s.size();
+        int r = n - 1;
+        while(l < r)
+        {
+            if(!isalnum(s[l]))
+            {
+                l++;
+                continue;
+            }
+            else if(!isalnum(s[r]))
+            {
+                r--;
+                continue;
+            }
+            else if(tolower(s[l]) != tolower(s[r])) return false; // only bracket was missing
+            else
+            {
+                l++;
+                r--;
+            }
+        }
+            return true;
+        }
+    
+};
+/*isalnum function in C++ is used to check if a character is alphanumeric,
+ tolower() Function*/
