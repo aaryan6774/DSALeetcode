3e65508d1993082077d82ac4f2d15eb32d1274be
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2025-03-10 21:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (94.32%) - LeetSync

---
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
@@ -0,0 +1,33 @@
+/*
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string answer = " ";
+        int n = words.size();
+        for(int i = 0; i<n; i++)
+        {
+            answer += words[i];
+            if(answer == s) {
+                return true;
+                break;
+            }
+            if(answer.length()>s.length()) return false;
+        }
+        return false;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool isPrefixString(string s, vector<string>& words) {
+        string ans = "";
+
+        for(auto& word:words){
+            ans += word;
+            if(ans == s) return true;
+            if(ans.length()>s.length()) return false;
+        }
+        return false;
+    }
+};
